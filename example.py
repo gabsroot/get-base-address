1f92b98f7ff09d86333054e09666b58490cb6e3d
gabsroot <101952389+gabsroot@users.noreply.github.com>
2024-03-07 01:56:07 -0300

    Update example.py

--- example.py
+++ example.py
@@ -2,21 +2,22 @@ from ReadWriteMemory import *
 from baseAddress import *
 
 # name of the process you want to handle
-PROCESS_NAME = "Multi Theft Auto.exe"
+PROCESS_NAME = "proccess.exe"
 
 rwm = ReadWriteMemory()
 base_address = BaseAddress(PROCESS_NAME)
 
+# opening the process
 process = rwm.get_process_by_name(PROCESS_NAME)
 process.open()
 
-# getting pointer from base address + offsets
-# replace address '0x00034A10' and offsets with the desired one
-pointer = process.get_pointer(base_address.get() + 0x00034A10, offsets=[0x38, 0x30, 0x34, 0x0, 0x0, 0x1C, 0xDB8])
+# getting base address pointer + offset array
+# replace address '0x00000000' and offsets
+pointer = process.get_pointer(base_address.get() + 0x00000000, offsets=[0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F])
 
 # reading the pointer value
 value = process.read(pointer)
 print(f"value: {value}")
 
-# overriding the pointer value by 10 (int)
+# overriding the pointer value by 10 <int>
 process.write(pointer, 10)
